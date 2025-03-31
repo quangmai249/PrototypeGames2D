ba1b6bd8b943e58f83943a6b432dadfdfd620bb9
Mai Quang <quangmai249@gmail.com>
2025-03-31 18:11:05 +0700

    Created : Gold Miner Ignore : Random Item and Particle System when Start bomb

--- Assets/Script/GoldMiner/ItemgoldMinerMoving.cs
+++ Assets/Script/GoldMiner/ItemgoldMinerMoving.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemgoldMinerMoving : MonoBehaviour
+public class ItemGoldMinerMoving : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool isFlip;
+    [SerializeField] float speed = 1;
+    [SerializeField] float maxDistance = 5;
+    [SerializeField] Vector3 direct;
+    [SerializeField] AudioClip clip;
+
+    private float distance;
+    private Vector3 defaultPos;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
     {
-        
+        defaultPos = transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        distance = Vector2.Distance(defaultPos, this.transform.position);
+
+        if (isFlip)
+        {
+            this.transform.Translate(speed * direct * Time.deltaTime);
+
+            if (distance >= maxDistance)
+            {
+                spriteRenderer.flipX = !spriteRenderer.flipX;
+                isFlip = false;
+            }
+        }
+        else
+        {
+            this.transform.Translate(-speed * direct * Time.deltaTime);
+
+            if (distance <= 0.5f)
+            {
+                spriteRenderer.flipX = !spriteRenderer.flipX;
+                isFlip = true;
+            }
+        }
+    }
+
+    public AudioClip Clip
+    {
+        get => this.clip;
     }
 }
--- Assets/Script/Scenes/GoldMinerScene.cs
+++ Assets/Script/Scenes/GoldMinerScene.cs
@@ -1,31 +1,93 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GoldMinerScene : MonoBehaviour
 {
-    [SerializeField] GameObject instruction;
+    [Header("Stats")]
+    [SerializeField] int numBombs = 3;
+    [SerializeField] int maxScore = 1000;
+    [SerializeField] float maxTime = 60;
+    [SerializeField] Slider sliderTime;
 
+    [Header("UI")]
+    [SerializeField] GameObject tap;
+    [SerializeField] GameObject instruction;
+    [SerializeField] GameObject panelEndGame;
+    [SerializeField] TextMeshProUGUI txtEndGame;
+    [SerializeField] TextMeshProUGUI txtNumBombs;
+    [SerializeField] TextMeshProUGUI txtTarget;
+
+    private PlayerGoldMiner playerGoldMiner;
     private void Awake()
     {
         AudioManager.Instance.PlayMusicBakground(SceneManager.GetActiveScene().name);
+        playerGoldMiner = GameObject.FindGameObjectWithTag(TagName.TAG_PLAYER).GetComponent<PlayerGoldMiner>();
+
+        panelEndGame.SetActive(false);
     }
 
     private void Start()
     {
-        StartCoroutine(nameof(CoroutineInstruction));
+        Time.timeScale = 1;
+        DOTween.timeScale = 1;
+
+        tap.transform.DOScale(new Vector3(.5f, .5f, .5f), 2f)
+          .SetLoops(-1, LoopType.Yoyo)
+          .SetEase(Ease.InOutSine);
+
+        instruction.transform.DOScale(new Vector3(0, 0, 0), 5);
+
+        sliderTime.value = maxTime;
+
+        txtNumBombs.text = numBombs.ToString();
+        txtTarget.text = $"Target : {maxScore}";
     }
 
-    private IEnumerator CoroutineInstruction()
+    private void Update()
     {
-        while (true)
+        if (playerGoldMiner.Score >= maxScore || maxTime <= 0)
         {
-            yield return new WaitForSeconds(1f);
-            instruction.SetActive(false);
+            if (playerGoldMiner.Score >= maxScore)
+                txtEndGame.text = "Win Game";
+            else
+                txtEndGame.text = "Lose Game";
 
-            yield return new WaitForSeconds(.25f);
-            instruction.SetActive(true);
+            Time.timeScale = 0;
+            DOTween.timeScale = 0;
+
+            panelEndGame.SetActive(true);
         }
+
+        maxTime -= Time.deltaTime;
+        sliderTime.value = maxTime;
+    }
+
+    public void ButtonBomb()
+    {
+        if (Time.timeScale == 0)
+            return;
+
+        if (numBombs > 0 && playerGoldMiner.GoItem != null)
+        {
+            Destroy(playerGoldMiner.GoItem);
+            numBombs--;
+            txtNumBombs.text = numBombs.ToString();
+
+            AudioManager.Instance.BombSound();
+        }
+    }
+
+    public void ButtonReplay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ButtonHome()
+    {
+        SceneManager.LoadScene(TagName.NAME_SCENE_HOME);
+        CanvasController.Instance.ActivePanel(TagName.PANEL_HOME_SCENE);
     }
 }
